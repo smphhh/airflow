943c47313fb6207b3ab33c5a4ff29866a95c5f88
Samuli Holopainen <smphhh@gmail.com>
2018-05-08 17:19:02 +0300

    Fix extraneous session commit in Dag TI slots dependency check

--- airflow/models.py
+++ airflow/models.py
@@ -3570,9 +3570,8 @@ class DAG(BaseDag, LoggingMixin):
     def owner(self):
         return ", ".join(list(set([t.owner for t in self.tasks])))
 
-    @property
     @provide_session
-    def concurrency_reached(self, session=None):
+    def check_concurrency_reached(self, session=None):
         """
         Returns a boolean indicating whether the concurrency limit for this DAG
         has been reached
@@ -3583,6 +3582,15 @@ class DAG(BaseDag, LoggingMixin):
             TI.state == State.RUNNING,
         )
         return qry.scalar() >= self.concurrency
+
+    @property
+    @provide_session
+    def concurrency_reached(self, session=None):
+        """
+        Returns a boolean indicating whether the concurrency limit for this DAG
+        has been reached
+        """
+        return self.check_concurrency_reached(session)
 
     @property
     @provide_session
