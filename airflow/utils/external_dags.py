60f2ee28dccb0dc747e373d1a1e9ef53ee50b411
Samuli Holopainen <smphhh@gmail.com>
2020-03-09 11:12:21 +0200

    Add argument to get_external_dag_paths function

--- airflow/utils/external_dags.py
+++ airflow/utils/external_dags.py
@@ -3,6 +3,13 @@ import json
 import os
 
 def get_external_dag_paths(dag_dir_or_file_path):
+    """
+    Get paths to external DAG import files
+
+    When given a path to a directory, returns paths to external DAG import files within this
+    directory. When given a path to a file, returns the same path if the path points to a
+    external DAG import file.
+    """
     external_dag_config_path = _find_external_dag_config(dag_dir_or_file_path)
     if external_dag_config_path is not None:
         with open(external_dag_config_path, mode="rb") as f:
@@ -11,12 +18,15 @@ def get_external_dag_paths(dag_dir_or_file_path):
         external_dag_provider_module = importlib.import_module(
             external_dag_config["external_dag_provider"])
         return external_dag_provider_module.get_external_dag_paths(
-            dag_dir_or_file_path, external_dag_config["config"])
+            os.path.dirname(external_dag_config_path), dag_dir_or_file_path, external_dag_config["config"])
 
     else:
         return []
 
 def get_external_dag(dag_file_path):
+    """
+    Download all external DAG files for the DAG import file
+    """
     if dag_file_path is None or os.path.exists(dag_file_path):
         return
 
